795a906e458c7ee6cde7d14a74c7842cc0271a78
Michael McCarthy <mikem@radicalresearch.llc>
2023-06-21 20:31:53 -0700

    fixed multiparcel coding checked new parcel data - no parcels!

--- 2_preprocess_LA_parcels.R
+++ 2_preprocess_LA_parcels.R
@@ -8,7 +8,7 @@
 ##this is intended to avoid having to do it multiple times since it only updates quarterly
 ##Also now removes duplicate location APN parcels (~893 APNs)
 
-rm(list =ls()) # clear environment
+#rm(list =ls()) # clear environment
 #'%ni%' <- Negate('%in%') ## not in operator
 gc()
 
@@ -22,30 +22,39 @@ wd <- getwd()
 warehouse_dir <- paste0(wd, '/Warehouse_data')
 LA_raw <- paste0(warehouse_dir, '/LACounty_Parcels.gdb')
 LA_tidy <- paste0(warehouse_dir, '/LAfiltered_shp')
+#LA_raw2 <- paste0(warehouse_dir, '/LACounty.gdb')
 
 ## Import the data
 
 #Set minimum size for analysis in thousand sq.ft. for non-warehouse classified
-sq_ft_threshold <- 28000
+#sq_ft_threshold <- 28000
 
 #Import raw LA County parcel data
-#sf::st_layers(dsn = LA_raw)
+sf::st_layers(dsn = LA_raw)
 LA_parcels <- sf::st_read(dsn = LA_raw, quiet = TRUE, type = 3)
+#LA_parcels_row1 <- sf::st_read(dsn = paste0(warehouse_dir, '/Assessor_Parcel_Data.csv'),
+#                              query = 'SELECT * FROM "Assessor_Parcel_Data" WHERE FID = 1')
+# land-use codes 3300, 3310, 3320, 3330, 3340, possibly 1340
+# open storage = 3900
+
+# UseCode_2 is 33 and UseDescription is 'Warehousing, Distribution, Storage' are identical
+# Open storage is NOT included - useCode 39
 
 LA_warehouse_parcels <- LA_parcels %>%
   filter(UseDescription == 'Warehousing, Distribution, Storage') 
 
 rm(ls = LA_parcels)
 gc()
+pryr::mem_used()
 
-#Filter only warehouses above 28k sq.ft.
-LA_industrial_28k_parcels <- LA_warehouse_parcels %>%
+#Tidy the dataset but don't filter on size yet
+LA_industrial_parcels <- LA_warehouse_parcels %>%
   #filter(UseType == 'Industrial') %>%
   mutate(type = ifelse(str_detect(str_to_lower(UseDescription), 'warehous'), 'warehouse', 
                        ifelse(str_detect(str_to_lower(UseDescription), 'industrial'), 'industrial', 'other')
   )
   ) %>%
-  filter(Shape_Area >= sq_ft_threshold) %>% 
+  #filter(Shape_Area >= sq_ft_threshold) %>% 
   select(APN, YearBuilt1, Shape_Area, type, Shape, UseDescription, 
          #SitusAddress, SitusCity, SitusZIP
          ) %>%
@@ -63,14 +72,48 @@ LA_industrial_28k_parcels <- LA_warehouse_parcels %>%
 rm(ls = LA_warehouse_parcels)
 gc()
 
-u <- st_equals(LA_industrial_28k_parcels, retain_unique = TRUE)
-unique <- LA_industrial_28k_parcels[-unlist(u),] %>% 
-  st_set_geometry(value = NULL)
+Geo_only <- LA_industrial_parcels |> 
+  select(Shape)
 
-final_LA <- unique %>% 
-  left_join(LA_industrial_28k_parcels)
+sf_use_s2(FALSE)
 
-rm(ls = u, unique, LA_industrial_28k_parcels)
+uniqueParcel <- Geo_only |> 
+  unique.data.frame() |> 
+  mutate(row = row_number()) |> 
+  st_make_valid()
+
+LA_interMulti <- uniqueParcel |> 
+  st_join(LA_industrial_parcels, join = st_equals) |> 
+  st_set_geometry(value = NULL) |> 
+  group_by(row) |>
+  summarize(count = n()) |> 
+  filter(count > 1) |> 
+  left_join(uniqueParcel, by = 'row') |> 
+  st_as_sf()
+
+LA_unique <- uniqueParcel |> 
+  st_join(LA_industrial_parcels, join = st_equals) |> 
+  st_set_geometry(value = NULL) |> 
+  group_by(row) |>
+  summarize(count = n()) |> 
+  filter(count == 1) |> 
+  left_join(uniqueParcel, by = 'row') |> 
+  st_as_sf() |> 
+  st_join(LA_industrial_parcels, join = st_equals) |> 
+  select(-row)
+ 
+LA_Multi <- LA_interMulti |> 
+  st_join(LA_industrial_parcels, join = st_equals) |> 
+  group_by(row, count, Shape, shape_area, class, type) |> 
+  summarize(apn = max(apn), year_built = max(year_built), .groups = 'drop') |> 
+  select(-row)
+
+final_LA <- bind_rows(LA_unique, LA_Multi)
+
+rm(ls = u, unique, LA_industrial_28k_parcels, LA_geometry, LA_industrial_parcels_10k,
+   LA_parcels_precise, unique2, Geo_only, LA_warehouse_parcels,
+   LA_industrial_parcels, LA_Multi, LA_unique, LA_interMulti)  
+   
 
 setwd(LA_tidy)
 unlink('LA_filtered_parcels.shp')
@@ -78,3 +121,4 @@ st_write(final_LA, 'LA_filtered_parcels.shp')
 #st_write(final_LA, 'LA_address.geojson')
 
 setwd(wd)
+
